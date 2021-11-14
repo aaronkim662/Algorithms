ee11344612563f692e26e3eebcdf47759eef14a0
aaron <aaronkim662@gmail.com>
2021-11-14 14:36:26 -0500

    edited bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,19 +1,24 @@
-const sorting = (arr) => {
-  let turn = true;
-  while(turn){
-    turn = false
-    for(let i = 0; i < arr.length - 1; i += 1){
-          console.log('index', i , arr[i], arr[i+1])
+// comparing the first two elements
+// compare first element throughout elements in the array
+// smallest number must be in the 0th index
+// increment first pointer and check again
+// second pointer is incremented by i + 1...
+// Big O -> 0 (n^2)
 
-      if(arr[i] > arr[i+1]){
-        // let temp = arr[i];
-        // arr[i] = arr[i+1];
-        // arr[i+1] = temp
-        [arr[i], arr[i+1]] = [arr[i+1], arr[i]]
-        turn = true
+const bubble = (arr) => {
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      let swap1 = arr[i]
+      let swap2 = arr[j]
+      if(swap2 < swap){
+        arr[i] = swap2
+        arr[j] = swap
       }
     }
   }
   return arr
+
 }
-console.log('bubble', sorting([1,4,6,3,9,11,4,20,75,9,-5]))
+
+console.log(bubble([6,4,2,3,1]))
+console.log(bubble([1,4,6,3,9,11,4,20,75,9,-5]))
