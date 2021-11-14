9cf90a46f399b344681bd422ed8a0c6096c2346a
aaron <aaronkim662@gmail.com>
2021-11-14 16:15:37 -0500

    edited selection sort

--- sorting/selection.js
+++ sorting/selection.js
@@ -1,4 +1,3 @@
-
 // selection sort
 // swap numbers based on lowest to high
 // nested for loop
@@ -6,18 +5,16 @@
 // RETURN original array sorted
 
 const selection = (arr) => {
-  for(let i = 0; i < arr.length;i++){
-    console.log('this is arr', i , arr)
-    for(let j = i+ 1; j< arr.length; j ++){
-      if(arr[i] > arr[j]){
+  for(let i = 0; i < arr.length; i++){
+    let min = arr[i]
+    for(let j = 1 + i; j < arr.length; j++){
+      if(arr[j] < min){
         [arr[i], arr[j]] = [arr[j], arr[i]]
-        // let temp = arr[i]
-        // arr[i] = arr[j]
-        // arr[j] = temp
       }
     }
-   }
-   return arr
+  }
+  return arr
 }
 
+// console.log(selection([1,3,2]))
 console.log(selection([5,4,3,2,1,0,-1]))
